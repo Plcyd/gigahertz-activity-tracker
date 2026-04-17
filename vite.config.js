7a934bffebbea6636b3cfdf773228c544671516e
PIOLO JANDA <117271930+Plcyd@users.noreply.github.com>
2026-04-17 16:00:52 +0800

    commited this to update and intergrate

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ jsxRuntime: 'automatic' })],
+  server: { port: 3000 },
+  build: { 
+    target: 'ES2020',
+    minify: 'terser'
+  }
 })
